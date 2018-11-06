3818521931fca5b32b588f329a37cf22d23d7f82
Das Wegas <daswegas@firma-solutions.com>
2018-11-06 12:38:51 +0000

    Do not direct to recently even if user is logged in

--- app/web/components/check-mnemonic.page.js
+++ app/web/components/check-mnemonic.page.js
@@ -37,9 +37,6 @@ export default class extends React.Component {
     }
 
     componentWillReceiveProps(props){
-        if(!!props.user_info){
-            return history.push("/recently")
-        }
     }
     
     onClickLogin = async()=>{
@@ -62,44 +59,45 @@ export default class extends React.Component {
         await window.hideIndicator()
     }
 
-    render_login() {
-		return (<div className="default-page login-page">
-            <div className="logo">
-                <img src="/static/logo_blue.png" onClick={()=>history.push("/")}/>
-            </div>
-            <div className="back-key">
-                <div className="round-btn" onClick={()=>history.goBack()}><i className="fas fa-arrow-left"></i></div>
-            </div>
-            <div className="container">
-                <h1>로그인</h1>
-                <div className="page">
-                    <div className="column-300">
-                        <div className="form-layout">
-                            <div className="form-label"> ID </div>
-                            <div className="form-input">
-                                <input placeholder="ID를 입력해주세요." value={this.state.user_id || ""} onChange={e=>this.setState({user_id:e.target.value})} />
-                            </div>
-                            
-                            <div className="form-label"> 비밀번호 </div>
-                            <div className="form-input">
-                                <input type="password" placeholder="비밀번호를 입력해주세요." value={this.state.password || ""} onKeyDown={this.keyPress} onChange={e=>this.setState({password:e.target.value})} />
-                            </div>
+    render_content() {
+        if(this.state.step == 0){
+            return this.render_login();
+        }else if(this.state.step == 1){
+            return this.render_masterkey();
+        }
+    }
 
-                            <div className="form-submit">
-                                {!localStorage.getItem("browser_key") || localStorage.getItem("browser_key_virgin") == 1 ? [
-                                    <button key={1} tabIndex={1} onClick={()=>history.push("/recover")}> 다른 계정으로 로그인 하기 </button>,
-                                    <button key={2} tabIndex={1} onClick={()=>history.push("/regist")}> 회원가입 </button>
-                                ] : [
-                                    <button key={1} tabIndex={1} onClick={this.onClickRecoverAccount}> 다른 계정으로 로그인 하기 </button>,
-                                    <button key={2} tabIndex={2} onClick={this.onClickClearBrowserKey}> 브라우저 인증 해제하기 </button>
-                                ]}
-                                <button className="border" onClick={this.onClickLogin}> 로그인 </button>
-                            </div>
+    render_login() {
+		return (
+            <div className="page">
+                <div className="column-300">
+                    <div className="form-layout">
+                        <div className="form-label"> ID </div>
+                        <div className="form-input">
+                            <input placeholder="ID를 입력해주세요." value={this.state.user_id || ""} onChange={e=>this.setState({user_id:e.target.value})} />
+                        </div>
+                        
+                        <div className="form-label"> 비밀번호 </div>
+                        <div className="form-input">
+                            <input type="password" placeholder="비밀번호를 입력해주세요." value={this.state.password || ""} onKeyDown={this.keyPress} onChange={e=>this.setState({password:e.target.value})} />
+                        </div>
+
+                        <div className="form-submit">
+                            {!localStorage.getItem("browser_key") || localStorage.getItem("browser_key_virgin") == 1 ? [
+                                <button key={1} tabIndex={1} onClick={()=>history.push("/recover")}> 다른 계정으로 로그인 하기 </button>,
+                                <button key={2} tabIndex={1} onClick={()=>history.push("/regist")}> 회원가입 </button>
+                            ] : [
+                                <button key={1} tabIndex={1} onClick={this.onClickRecoverAccount}> 다른 계정으로 로그인 하기 </button>,
+                                <button key={2} tabIndex={2} onClick={this.onClickClearBrowserKey}> 브라우저 인증 해제하기 </button>
+                            ]}
+                            <button className="border" onClick={this.onClickLogin}> 로그인 </button>
                         </div>
                     </div>
                 </div>
             </div>
-		</div>);
+        );
+
+
     }
 
     render_masterkey(){
@@ -133,11 +131,17 @@ export default class extends React.Component {
             return <div />
         }
 
-        if(this.state.step == 0){
-            return this.render_login();
-        }else if(this.state.step == 1){
-            return this.render_masterkey();
-        }
-
+		return (<div className="default-page login-page">
+            <div className="logo">
+                <img src="/static/logo_blue.png" onClick={()=>history.push("/")}/>
+            </div>
+            <div className="back-key">
+                <div className="round-btn" onClick={()=>history.goBack()}><i className="fas fa-arrow-left"></i></div>
+            </div>
+            <div className="container">
+                <h1>마스터 키워드 확인</h1>
+                {this.render_content()}
+            </div>
+		</div>);
 	}
 }
