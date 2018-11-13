79b0f406d5d2570692135db60e3747b848df0477
Das Wegas <daswegas@firma-solutions.com>
2018-11-13 02:28:20 +0000

    Alert no folder earlier

--- app/web/components/contract-list.page.js
+++ app/web/components/contract-list.page.js
@@ -102,6 +102,13 @@ export default class extends React.Component {
     }
 
     onClickMoveMode=()=>{
+        await window.showIndicator();
+        let list = await this.props.all_folders();
+        await window.hideIndicator();
+        if(list.length == 0){
+            return alert("생성된 폴더가 없습니다.")
+        }
+
         this.setState({
             moveMode:true,
             move_select:[]
