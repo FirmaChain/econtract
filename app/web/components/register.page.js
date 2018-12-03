814e30a1f2d606bcad471716f327c7d99526a6e5
PBES <pbes0707@gmail.com>
2018-12-04 07:24:37 +0900

    footer

--- app/web/components/register.page.js
+++ app/web/components/register.page.js
@@ -785,6 +785,15 @@ export default class extends React.Component {
                     {this.render_content()}
                 </div>
             </div>
+            <div className="footer">
+                <div className="left">Copyright 2018 Firma Solutions, Inc, All right reserved</div>
+                <div className="middle">
+                    이용약관 | 개인정보처리방침
+                </div>
+                <div className="right">
+                    developer@firma-solutions.com
+                </div>
+            </div>
 		</div>);
 	}
 }
