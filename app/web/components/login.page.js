0529dc17a7376cba0424f85d01acce54aa49b11d
PBES <pbes0707@gmail.com>
2018-12-12 15:02:32 +0900

    login modal add

--- app/web/components/login.page.js
+++ app/web/components/login.page.js
@@ -41,7 +41,7 @@ export default class extends React.Component {
         }
     }
     
-    onClickLogin = async()=>{
+    onClickLogin = async ()=>{
         await window.showIndicator()
         
         if (!this.state.email || !this.state.password) {
@@ -63,7 +63,7 @@ export default class extends React.Component {
         await window.hideIndicator()
     }
 
-    onClickClearBrowserKey = async()=>{
+    onClickClearBrowserKey = async ()=>{
         await window.showIndicator();
         if (window._confirm("신규 회원가입을 하시려면 브라우저 인증을 해제해야 합니다.\n브라우저 인증 해제 후 기존의 계정으로 다시 로그인 하시려면 [기존 계정으로 로그인] 과정을 거쳐야 하며,\n이 때 마스터 키워드가 필요합니다.\n\n브라우저 인증을 해제 하시겠습니까?")) { 
             localStorage.removeItem("browser_key");
@@ -74,7 +74,7 @@ export default class extends React.Component {
         await window.hideIndicator();
     }
 
-    onClickRecoverAccount = async() => {
+    onClickRecoverAccount = async () => {
         await window.showIndicator();
         if (window._confirm("다른 계정으로 로그인하기 전에 먼저 브라우저 인증을 해제해야 합니다.\n브라우저 인증 해제 후 기존의 계정으로 다시 로그인 하시려면 [기존 계정으로 로그인] 과정을 거쳐야 하며,\n이 때 마스터 키워드가 필요합니다.\n\n브라우저 인증을 해제 하시겠습니까?")) {
             localStorage.removeItem("browser_key");
@@ -85,11 +85,28 @@ export default class extends React.Component {
         await window.hideIndicator();
     }
 
-   keyPress = async(e) => {
-      if(e.keyCode == 13){
-        this.onClickLogin()
-      }
-   }
+    openVerifiedBrowserModal = () => {
+        window.openModal("CommonModal", {
+            icon:"fal fa-lock-alt",
+            title:"인증된 브라우저",
+            subTitle:"이컨트랙트 서비스는 마스터 키워드를 기반으로 로그인 하실 수 있습니다.",
+            desc:`회원가입시 발급되는 마스터 키워드는 해당 계정에 귀속되며, 당시 사용된 브라우저에 자동으로 저장됩니다.<br/><br/>
+회원님의 계정은 일치하는 마스터 키워드가 
+저장되있는 브라우저에서만 로그인이 가능합니다.<br/><br/>
+서비스에 가입한 적이 없다면 [신규 회원가입]을 통해 이컨트랙트 서비스를 경험해보시길 바랍니다.<br/><br/>
+현재 접속해있는 브라우저에 회원님의 마스터 키워드가 저장되어 있지 않은 경우, [다른 계정으로 로그인] 기능을 통해 현재 인증된 계정을 해제하고 회원님의 기존 
+마스터 키워드를 사용하여 로그인하실 수 있습니다.`,
+            onClose:()=>{
+                refesh_modal_idx = null
+            }
+        })
+    }
+
+    keyPress = async (e) => {
+        if(e.keyCode == 13){
+            this.onClickLogin()
+        }
+    }
 
    render_new() {
 		return (<div className="login-common-page first-login-page">
@@ -136,7 +153,7 @@ export default class extends React.Component {
             <div className="container">
                 <div className="title">E-Contract 시작하기</div>
                 <div className="desc1">접속하신 브라우저는 인증되었습니다.</div>
-                <div className="desc2">인증된 브라우저란?</div>
+                <div className="desc2" onClick={this.openVerifiedBrowserModal}>인증된 브라우저란?</div>
 
                 <div className="textbox"><input className="common-textbox" id="email" type="email" placeholder="이메일을 입력해주세요." value={this.state.email || ""} onChange={e=>this.setState({email:e.target.value})}/></div>
                 <div className="textbox"><input className="common-textbox" id="password" type="password" placeholder="비밀번호를 입력해주세요." value={this.state.password || ""} onKeyDown={this.keyPress} onChange={e=>this.setState({password:e.target.value})}/></div>
