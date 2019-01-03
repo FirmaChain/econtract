8323c54a51b0fef8581ff635d8273ceaaaa846ac
PBES <pbes0707@gmail.com>
2019-01-04 06:38:10 +0900

    qfqf

--- app/web/components/chatting.comp.js
+++ app/web/components/chatting.comp.js
@@ -104,7 +104,7 @@ export default class extends React.Component {
             case 2: {
                 // 계약서 내용 바꼈을때
                 let data = JSON.parse(e.msg)
-                let entity = this.state.infos.find(e=>e.entity_id == data.entity_id && e.corp_id == data.corp_id)
+                let entity = this.props.infos.find(e=>e.entity_id == data.entity_id && e.corp_id == data.corp_id)
                 let text = `${entity.user_info.name}님이 계약서를 수정하였습니다.`
                 return <div key={e.chat_id} className="notice">
                     {text}
@@ -113,7 +113,7 @@ export default class extends React.Component {
             case 3: {
                 // 서명 했을때
                 let data = JSON.parse(e.msg)
-                let entity = this.state.infos.find(e=>e.entity_id == data.entity_id && e.corp_id == data.corp_id)
+                let entity = this.props.infos.find(e=>e.entity_id == data.entity_id && e.corp_id == data.corp_id)
                 let text = `${entity.user_info.name}님이 해당 계약서에 서명하였습니다.`
                 return <div key={e.chat_id} className="notice">
                     {text}
@@ -122,7 +122,7 @@ export default class extends React.Component {
             case 4: {
                 // 서명 정보 바꿨을때
                 let data = JSON.parse(e.msg)
-                let entity = this.state.infos.find(e=>e.entity_id == data.entity_id && e.corp_id == data.corp_id)
+                let entity = this.props.infos.find(e=>e.entity_id == data.entity_id && e.corp_id == data.corp_id)
                 let text = `${entity.user_info.name}님이 서명에 필요한 정보를 수정하였습니다.`
                 return <div key={e.chat_id} className="notice">
                     {text}
@@ -131,8 +131,8 @@ export default class extends React.Component {
             case 5: {
                 // 수정 권한 옮겼을 때
                 let data = JSON.parse(e.msg)
-                let entity = this.state.infos.find(e=>e.entity_id == data.account_id && e.corp_id == 0)
-                let move_entity = this.state.infos.find(e=>e.entity_id == data.move_account_id && e.corp_id == 0)
+                let entity = this.props.infos.find(e=>e.entity_id == data.account_id && e.corp_id == 0)
+                let move_entity = this.props.infos.find(e=>e.entity_id == data.move_account_id && e.corp_id == 0)
                 let text = `${entity.user_info.name}님이 ${move_entity.user_info.name}님에게 수정 권한을 넘겼습니다.`
                 return <div key={e.chat_id} className="notice">
                     {text}
