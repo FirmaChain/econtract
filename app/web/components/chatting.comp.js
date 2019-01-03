2bd056b160f98d391c6ddb4f1b3e2b70d3265a6e
PBES <pbes0707@gmail.com>
2019-01-04 06:39:33 +0900

    remove log

--- app/web/components/chatting.comp.js
+++ app/web/components/chatting.comp.js
@@ -105,7 +105,6 @@ export default class extends React.Component {
                 // 계약서 내용 바꼈을때
                 let data = JSON.parse(e.msg)
                 let entity = this.props.infos.find(e=>e.entity_id == data.entity_id && e.corp_id == data.corp_id)
-                console.log(entity)
                 let text = `${entity.user_info.username}님이 계약서를 수정하였습니다.`
                 return <div key={e.chat_id} className="notice">
                     {text}
