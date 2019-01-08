b065ced50f4f87da1e4dd247b3a40d5e967033cd
PBES <pbes0707@gmail.com>
2019-01-08 22:15:41 +0900

    qfwe

--- app/web/components/chatting.comp.js
+++ app/web/components/chatting.comp.js
@@ -89,8 +89,9 @@ export default class extends React.Component {
                 let isMine = meOrGroup == user
 
                 let text
+                let data
                 try {
-                    let data = JSON.parse(e.msg)
+                    data = JSON.parse(e.msg)
                     text = data.text
                 } catch(err) {
                     text = e.msg
@@ -99,7 +100,7 @@ export default class extends React.Component {
                 return <div key={e.chat_id} className={isMine ? "chat-slot right" : "chat-slot left"}>
                     { !isMine ? <img className="profile" src={`https://identicon-api.herokuapp.com/${user.corp_id+"_"+user.entity_id}/70?format=png`}/> : null }
                     <div className="msg">
-                        <div className="name">{user.user_info.username ? user.user_info.username : (user.user_info.title + " " + e.msg.username)}</div>
+                        <div className="name">{user.user_info.username ? user.user_info.username : (user.user_info.title + " " + data.username)}</div>
                         <div className="msg-text">{text}</div>
                     </div>
                 </div>
