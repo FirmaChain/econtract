a12e26d9ffbc0d671f43eb58fc5f861be1c09a91
Das Wegas <daswegas@firma-solutions.com>
2019-01-08 09:24:18 +0000

    Add subscribe

--- app/common/actions/subscribe.js
+++ app/common/actions/subscribe.js
@@ -2,10 +2,6 @@ import {
     api_get_subscribe_plan,
 } from "../../../gen_api"
 
-export const GET_CONTRACTS = "GET_CONTRACTS";
-export const FOLDER_LIST_CONTRACT = "FOLDER_LIST_CONTRACT";
-const DUMMY_CORP_ID = 0;
-
 export function get_subscribe_plan(){
     return async function(){
         return (await api_get_subscribe_plan());
