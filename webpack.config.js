f46049b3df13e0bd2e23a1ef6af939e98400b6e2
PBES <pbes0707@gmail.com>
2019-01-23 04:06:30 +0900

    qfqf

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ console.log("build mode : ", NODE_ENV)
 
 plugins.push(new webpack.DefinePlugin({
   'process.env': {
-    'NODE_ENV': JSON.stringify(NODE_ENV),
+    'NODE_ENV': JSON.stringify("development"),
   }
 }))
 
