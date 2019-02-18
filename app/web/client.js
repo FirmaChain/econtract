0ba54baa92d9c7d8b01bcf70b6f3b5f770316160
PBES <pbes0707@gmail.com>
2019-02-18 15:20:24 +0900

    q1wfqwfqwfqwf

--- app/web/client.js
+++ app/web/client.js
@@ -68,8 +68,8 @@ window.addEventListener("load",()=>{
 		<Router history={history}>
 			<Provider store={store}>
 				<Container>
-					<Route onEnter={resolver} exact path="/login2" component={VerificationPage} />
-					<Route onEnter={resolver} exact path="/login2/:id" component={VerificationPage} />
+					<Route onEnter={resolver} exact path="/login2" component={LoginPage} />
+					<Route onEnter={resolver} exact path="/login2/:id" component={LoginPage} />
 
 					<Route onEnter={resolver} exact path="/" component={IndexPage} />
 					<Route onEnter={resolver} exact path="/login" component={LoginPage} />
