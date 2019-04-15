f5eb8c8a130eab543bfb1240ba629a157de0ac1a
PBES <pbes0707@gmail.com>
2019-04-16 06:59:52 +0900

    ㅃ

--- gen_api.js
+++ gen_api.js
@@ -826,15 +826,6 @@ export async function api_get_payment_info(){
         session:window.getCookie("session")
     });
 }
-export async function api_make_yearly_commitment(plan_id){
-    let __data = new FormData();
-
-    if(plan_id != null) __data.append('plan_id', plan_id)
-
-    return await post("/make_yearly_commitment", __data,{
-        session:window.getCookie("session")
-    });
-}
 export async function api_make_monthly_commitment(plan_id){
     let __data = new FormData();
 
