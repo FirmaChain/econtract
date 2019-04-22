7169a1f254d8806125d5aee137369fb7007a4c42
PBES <pbes0707@gmail.com>
2019-04-22 16:32:46 +0900

    qwfasfqw

--- app/web/components/custom_route.js
+++ app/web/components/custom_route.js
@@ -12,6 +12,7 @@ class MyRoute extends React.Component{
 		console.log(navigator.userAgent)
 		if(window.location.pathname != "/"){
 			if(navigator.userAgent.toLowerCase().indexOf("chrome") != -1 ||
+				navigator.userAgent.toLowerCase().indexOf("crios") != -1 ||
 			 	navigator.userAgent.toLowerCase().indexOf("ipad") != -1){
 				this.setState({ chrome:true })
 			}else{
